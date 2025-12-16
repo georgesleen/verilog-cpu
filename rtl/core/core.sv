15104d582ec1ab87aeb82eb3d583d8387e6e581f
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2025-12-15 22:13:41 -0800

    Implement very very simple core

--- rtl/core/core.sv
+++ rtl/core/core.sv
@@ -15,17 +15,92 @@ module core #(
     input logic [XLEN-1:0] memory_read_data
 );
 
-  /*
-  * Architectural state
-  */
-  logic [XLEN-1:0] pc_reg, pc_next;
-  logic [XLEN-1:0] registers[REGISTER_COUNT-1:0];
-  logic [XLEN-1:0] registers_next[REGISTER_COUNT-1:0];
-
-  assign program_counter = pc_reg;
+    /*
+    * Architectural state
+    */
+    logic [XLEN-1:0] program_counter_next;
+    logic [XLEN-1:0] registers[REGISTER_COUNT-1:0];
+    logic [XLEN-1:0] registers_next[REGISTER_COUNT-1:0];
 
     /*
     * Decode
     */
+    logic [OPCODE_FIELD_WIDTH-1:0] instruction_class;
+    logic [REGISTER_INDEX_WIDTH-1:0] destination_register_index;
+    logic [REGISTER_INDEX_WIDTH-1:0] source_register_1_index;
+    logic [REGISTER_INDEX_WIDTH-1:0] source_register_2_index;
+
+    /*
+    * Immediate values
+    */
+    logic [XLEN-1:0] immediate_i;
+    logic [XLEN-1:0] immediate_s;
+    logic [XLEN-1:0] immediate_j;
+
+    /*
+    * Decode logic
+    */
+    assign instruction_class = instruction[OPCODE_MSB:OPCODE_LSB];
+    assign destination_register_index = instruction[DEST_REG_MSB:DEST_REG_LSB];
+    assign source_register_1_index = instruction[SRC_REG1_MSB:SRC_REG1_LSB];
+    assign source_register_2_index = instruction[SRC_REG2_MSB:SRC_REG2_LSB];
+
+    assign immediate_i = {{(XLEN - 12) {instruction[31]}}, instruction[31:20]};
+    assign immediate_s = {{(XLEN - 12) {instruction[31]}}, instruction[31:25], instruction[11:7]};
+    assign immediate_j = {
+        {(XLEN - 12) {instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21]
+    };
+
+    /*
+    * Execute
+    */
+    always_comb begin
+        // Default behavior
+        program_counter_next = program_counter + INSTRUCTION_BYTES;
+        registers_next       = registers;
+        memory_write_enable  = 1'b0;
+        memory_address       = '0;
+        memory_write_data    = '0;
+
+        // Instruction specific
+        case (instruction_class)
+            // Integer arithmetic with immediate
+            CLASS_OP_IMM: begin
+                registers_next[destination_register_index] = registers[source_register_1_index] + immediate_i;
+            end
+            // Jump and link
+            CLASS_JAL: begin
+                registers_next[destination_register_index] = program_counter + INSTRUCTION_BYTES;
+                program_counter_next                       = program_counter + immediate_j;
+            end
+            // Store to memory
+            CLASS_STORE: begin
+                memory_write_enable = 1'b1;
+                memory_address      = registers[source_register_1_index] + immediate_s;
+                memory_write_data   = register_file[source_register_2_index];
+            end
+            default: begin
+                // nop
+            end
+        endcase
+
+        // Enforce register X0 = 0
+        registers_next[X0] = '0;
+    end
+
+    /*
+    * Commit
+    */
+    always_ff @(posedge clk) begin
+        if (~n_rst) begin
+            program_counter <= '0;
+            for (int i = 0; i < REGISTER_COUNT; i++) begin
+                registers[i] <= '0;
+            end
+        end else begin
+            program_counter <= program_counter_next;
+            registers       <= registers_next;
+        end
+    end
 
 endmodule
--- rtl/riscv_pkg.sv
+++ rtl/riscv_pkg.sv
@@ -5,27 +5,46 @@
 */
 package riscv_pkg;
 
-  // ISA specification
-  localparam int BYTE_WIDTH        = 8;
-  localparam int INSTRUCTION_WIDTH = 32;
-  localparam int INSTRUCTION_BYTES = INSTRUCTION_WIDTH / BYTE_WIDTH;
-  localparam int REGISTER_COUNT    = 32;
+    // ISA specification
+    localparam int BYTE_WIDTH = 8;
+    localparam int INSTRUCTION_WIDTH = 32;
+    localparam int INSTRUCTION_BYTES = INSTRUCTION_WIDTH / BYTE_WIDTH;
+    localparam int REGISTER_COUNT = 32;
 
-  // Instruction field widths
-  localparam int OPCODE_FIELD_WIDTH = 7;
+    // Field widths and indices
+    localparam int OPCODE_FIELD_WIDTH = 7;
+    localparam int REGISTER_INDEX_WIDTH = 5;
 
-  // Opcodes
-  localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_OP_IMM  = 7'b0010011;
-  localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_OP      = 7'b0110011;
-  localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_LOAD    = 7'b0000011;
-  localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_STORE   = 7'b0100011;
-  localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_BRANCH  = 7'b1100011;
-  localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_JAL     = 7'b0100011;
-  localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_JALR    = 7'b0100011;
+    localparam int OPCODE_MSB = 6;
+    localparam int OPCODE_LSB = 0;
 
-  // Registers
-  localparam int X0 = 0;
-  localparam int RA = 1;
-  localparam int SP = 2;
+    localparam int DEST_REG_MSB = 11;
+    localparam int DEST_REG_LSB = 7;
+
+    localparam int FUNCT3_MSB = 14;
+    localparam int FUNCT3_LSB = 12;
+
+    localparam int SRC_REG1_MSB = 19;
+    localparam int SRC_REG1_LSB = 15;
+
+    localparam int SRC_REG2_MSB = 24;
+    localparam int SRC_REG2_LSB = 20;
+
+    localparam int FUNCT7_MSB = 31;
+    localparam int FUNCT7_LSB = 25;
+
+    // Opcodes
+    localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_OP_IMM = 7'b0010011;
+    localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_OP = 7'b0110011;
+    localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_LOAD = 7'b0000011;
+    localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_STORE = 7'b0100011;
+    localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_BRANCH = 7'b1100011;
+    localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_JAL = 7'b0100011;
+    localparam logic [OPCODE_FIELD_WIDTH-1:0] CLASS_JALR = 7'b0100011;
+
+    // Registers
+    localparam int X0 = 0;
+    localparam int RA = 1;
+    localparam int SP = 2;
 
 endpackage
